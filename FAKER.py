471ac41e8fdc46454f976bc1017c56bc014f842e
Praveen kanth <145778653+Praveenkanth10@users.noreply.github.com>
2024-08-02 20:54:38 +0530

    Update FAKER.py
    Faker is a Python library that enables data scientists and analysts to generate synthetic data for testing, development, and analysis purposes. It provides a range of methods to generate realistic fake data, such as names, addresses, phone numbers, email addresses, dates, and more.

--- FAKER.py
+++ FAKER.py
@@ -1,1 +1,30 @@
-#install the 
+#install the Required Library
+
+!pip install Faker
+
+from faker import Faker 
+
+fake = Faker()
+
+#storing the values into the variable
+people = []
+
+#allcating the index of the value
+for i in range(50):
+    name = fake.name() 
+    age = fake.random_int(min=18, max=50)
+    phone = fake.phone_number()
+    people.append([name, age, phone])  
+
+for person in people:
+  print(f"name: {person[0]}, age: {person[1]}, phone: {person[2]}") 
+
+#Coverting the data into the excel format
+import pandas as pd
+df=pd.DataFrame(people,columns=['name','age','phone'])
+df.to_excel("people.xlsx",index=False)
+
+#Downloading the sample data into the excel file
+from google.colab import files
+files.download('people.xlsx')
+
